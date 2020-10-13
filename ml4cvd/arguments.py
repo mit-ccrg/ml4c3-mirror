80ab2829bf6bac746cee30e801d2ffad08158cd1
Erik Reinertsen <ereinertsen@mgh.harvard.edu>
2020-10-13 16:58:44 -0400

    change format to PDF (#570)

--- ml4cvd/arguments.py
+++ ml4cvd/arguments.py
@@ -42,8 +42,8 @@ def parse_args() -> argparse.Namespace:
     # Image file format arguments
     parser.add_argument(
         "--image_ext",
-        default=".eps",
-        choices=[".eps", ".svg", ".png"],
+        default=".pdf",
+        choices=[".pdf", ".eps", ".svg", ".png"],
         help="File format extension to save images as. Note this includes a leading period.",
     )
 
