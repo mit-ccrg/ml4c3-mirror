81ddec9e7ca6f7cf5e8744174dee20029b1e0798
Erik Reinertsen <ereinertsen@mgh.harvard.edu>
2020-11-09 20:07:03 -0500

    Minor updates to deidentify.py (#767)
    * update de-id
    
    * rename sts_skip

--- scripts/deidentify.py
+++ scripts/deidentify.py
@@ -61,10 +61,10 @@ phi_keys = {
 }
 
 
-def _get_ecg_mrns(args):
+def _get_hd5_mrns(args):
     mrns = set()
-    if args.new_ecg_dir is not None:
-        for root, dirs, files in os.walk(args.ecg_dir):
+    if args.path_to_hd5_deidentified is not None:
+        for root, dirs, files in os.walk(args.path_to_hd5):
             for file in files:
                 split = os.path.splitext(file)
                 if split[-1] != TENSOR_EXT:
@@ -78,29 +78,39 @@ def _get_ecg_mrns(args):
     return mrns
 
 
-sts_skip = set()
+path_of_csv_to_skip = set()
 
 
-def _get_sts_mrns(args):
+def _get_csv_mrns(args):
     mrns = set()
-    if args.new_sts_dir is not None:
-        for root, dirs, files in os.walk(args.sts_dir):
-            for file in files:
-                split = os.path.splitext(file)
-                if split[-1] != CSV_EXT:
-                    continue
-                path = os.path.join(root, file)
-                try:
-                    _mrns = _sample_csv_to_set(sample_csv=path)
-                except ValueError:
-                    print(
-                        f"Could not get MRNs from csv, skipping de-identification: {path}",
-                    )
-                    global sts_skip
-                    sts_skip.add(path)
-                    continue
-                _mrns = {int(mrn) for mrn in _mrns}
-                mrns |= _mrns
+    if args.path_to_csv_deidentified is not None:
+
+        # Get list of full paths to CSV files
+        fpaths = []
+        if os.path.isdir(args.path_to_csv):
+            for root, dirs, fnames in os.walk(args.path_to_csv):
+                for fname in fnames:
+                    split = os.path.splitext(fname)
+                    if split[-1] != CSV_EXT:
+                        continue
+                    fpath = os.path.join(root, fname)
+                    fpaths.append(fpath)
+        # If user gave path to single CSV, instead of a directory, use that path
+        else:
+            fpaths.append(args.path_to_csv)
+
+        # Iterate over paths to CSV files
+        for fpath in fpaths:
+            try:
+                _mrns = _sample_csv_to_set(sample_csv=fpath)
+            except ValueError:
+                print(f"Could not get MRNs from {fpath}, skipping de-identification")
+                global path_of_csv_to_skip
+                path_of_csv_to_skip.add(fpath)
+                continue
+            _mrns = {int(mrn) for mrn in _mrns}
+            mrns |= _mrns
+
     return mrns
 
 
@@ -109,12 +119,9 @@ def _get_mrns(args, skip_mrns=set()):
     Get a list of unique MRNs from the data sources that are being remapped.
     """
     mrns = set()
-
-    mrns |= _get_ecg_mrns(args)
-    mrns |= _get_sts_mrns(args)
-
+    mrns |= _get_hd5_mrns(args)
+    mrns |= _get_csv_mrns(args)
     mrns -= skip_mrns
-
     return mrns
 
 
@@ -125,8 +132,8 @@ def _remap_mrns(args):
     mrn_map = dict()
     starting_id = args.starting_id
     if os.path.isfile(args.mrn_map):
-        # call to _get_sts_mrns to determine which files to skip for sts deidentification
-        _get_sts_mrns(args)
+        # call to _get_csv_mrns to determine which files to skip for sts deidentification
+        _get_csv_mrns(args)
 
         mrn_map = pd.read_csv(args.mrn_map, low_memory=False, usecols=["mrn", "new_id"])
         mrn_map = mrn_map.set_index("mrn")
@@ -134,9 +141,6 @@ def _remap_mrns(args):
         if starting_id is None:
             starting_id = max(mrn_map.values()) + 1
         print(f"Existing MRN map loaded from {args.mrn_map}")
-
-    if starting_id is None:
-        starting_id = 1
 
     new_mrns = _get_mrns(args, skip_mrns=set(mrn_map.keys()))
     new_ids = list(range(starting_id, len(new_mrns) + starting_id))
@@ -171,9 +175,10 @@ def _swap_path_prefix(path, prefix, new_prefix):
     return new_path
 
 
-def _deidentify_ecg(old_new_path):
+def _deidentify_hd5(old_new_path):
     """
-    Given a path to an existing HD5, copy it to a new path and delete all identifiable information.
+    Given a path to an existing HD5, copy it to a new path and delete all identifiable
+    information. Currently only set up for ECG data.
     """
     old_path, new_path = old_new_path
     if os.path.exists(new_path):
@@ -192,16 +197,20 @@ def _deidentify_ecg(old_new_path):
             hd5.create_dataset("deidentified", data=True, dtype=bool)
 
 
-def _deidentify_ecgs(args, mrn_map):
+def _deidentify_hd5s(args, mrn_map):
     """
-    Create de-identified ECGs in parallel.
+    Create de-identified HD5 files in parallel.
     """
-    if args.new_ecg_dir is None:
+    if args.path_to_hd5_deidentified is None:
         return
 
     old_new_paths = []
-    for root, dirs, files in os.walk(args.ecg_dir):
-        new_root = _swap_path_prefix(root, args.ecg_dir, args.new_ecg_dir)
+    for root, dirs, files in os.walk(args.path_to_hd5):
+        new_root = _swap_path_prefix(
+            root,
+            args.path_to_hd5,
+            args.path_to_hd5_deidentified,
+        )
         for file in files:
             split = os.path.splitext(file)
             if split[-1] != TENSOR_EXT:
@@ -217,14 +226,14 @@ def _deidentify_ecgs(args, mrn_map):
             old_new_paths.append((old_path, new_path))
 
     with Pool(processes=args.num_workers) as pool:
-        pool.map(_deidentify_ecg, old_new_paths)
+        pool.map(_deidentify_hd5, old_new_paths)
 
-    print(f"De-identified {len(old_new_paths)} ECGs at {args.new_ecg_dir}")
+    print(f"De-identified {len(old_new_paths)} ECGs at {args.path_to_hd5_deidentified}")
 
 
-def _deidentify_sts_csv(path, mrn_map):
+def _deidentify_csv(path, mrn_map):
     """
-    Given a path to an STS CSV, delete all identifiable information.
+    Given a path to a CSV, delete all identifiable information.
     """
     df = pd.read_csv(path, header=None, low_memory=False)
 
@@ -255,16 +264,20 @@ def _deidentify_sts_csv(path, mrn_map):
     df.to_csv(path, index=False)
 
 
-def _deidentify_sts_csvs(args, mrn_map):
+def _deidentify_csvs(args, mrn_map):
     """
     Create de-identified STS data.
     """
-    if args.new_sts_dir is None:
+    if args.path_to_csv_deidentified is None:
         return
 
     count = 0
-    for root, dirs, files in os.walk(args.sts_dir):
-        new_root = _swap_path_prefix(root, args.sts_dir, args.new_sts_dir)
+    for root, dirs, files in os.walk(args.path_to_csv):
+        new_root = _swap_path_prefix(
+            root,
+            args.path_to_csv,
+            args.path_to_csv_deidentified,
+        )
         for file in files:
             split = os.path.splitext(file)
             if split[-1] != CSV_EXT:
@@ -275,23 +288,20 @@ def _deidentify_sts_csvs(args, mrn_map):
                 os.remove(new_path)
             shutil.copyfile(old_path, new_path)
             # if there was no PHI in the original file, copy it without trying to deidentify
-            global sts_skip
-            if old_path in sts_skip:
+            global path_of_csv_to_skip
+            if old_path in path_of_csv_to_skip:
                 continue
-            _deidentify_sts_csv(new_path, mrn_map)
+            _deidentify_csv(new_path, mrn_map)
             count += 1
 
-    print(f"De-identified {count} STS CSVs at {args.new_sts_dir}")
+    print(f"De-identified {count} CSV files at {args.path_to_csv_deidentified}")
 
 
 def run(args):
     start_time = timer()
-
     mrn_map = _remap_mrns(args)
-
-    _deidentify_ecgs(args, mrn_map)
-    _deidentify_sts_csvs(args, mrn_map)
-
+    _deidentify_hd5s(args, mrn_map)
+    _deidentify_csvs(args, mrn_map)
     end_time = timer()
     elapsed_time = end_time - start_time
     print(f"De-identification took {elapsed_time:.2f} seconds.")
@@ -301,37 +311,37 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--mrn_map",
-        default=os.path.expanduser("~/dropbox/ecg/mgh-mrn-deid-map.csv"),
-        help="Path to CSV of MRN -> deidentified ID map.",
+        default=os.path.expanduser("~/dropbox/mrn-deid-maps/mgh.csv"),
+        help="Path to CSV in which to store map from MRN -> deidentified ID.",
     )
     parser.add_argument(
         "--starting_id",
         type=int,
+        default=1,
         help="Starting value for new IDs.",
     )
     parser.add_argument(
-        "--ecg_dir",
+        "--path_to_hd5",
         default="/storage/shared/ecg/mgh",
-        help="Path to ECG HD5s.",
+        help="Path to directory containing hd5 files.",
     )
     parser.add_argument(
-        "--sts_dir",
-        default=os.path.expanduser("~/dropbox/sts-data"),
-        help="Path to STS CSVs.",
-    )
-    parser.add_argument(
-        "--new_ecg_dir",
+        "--path_to_hd5_deidentified",
         help="Path to save de-identified ECG HD5s to. "
         "Skip this argument to skip de-identification of ECG data.",
     )
     parser.add_argument(
-        "--new_sts_dir",
-        help="Path to save de-identified STS CSVs to. "
+        "--path_to_csv",
+        help="Path to directory of CSV files, or CSV file.",
+    )
+    parser.add_argument(
+        "--path_to_csv_deidentified",
+        help="Path to save de-identified CSVs to. "
         "Skip this argument to skip de-identification of STS data.",
     )
     parser.add_argument(
         "--num_workers",
-        default=cpu_count(),
+        default=cpu_count() - 1,
         type=int,
         help="Number of worker processes to use if processing in parallel.",
     )
