de451c2660fe2423c2c9882a00f026528ff5c42f
ndiamant <ndiamant@broadinstitute.org>
2020-11-17 15:54:27 -0500

    bug fixes, augmention strengths calibrated

--- ecg_regnet/data.py
+++ ecg_regnet/data.py
@@ -1,13 +1,13 @@
 # Imports: standard library
+import os
 import argparse
 from typing import Dict, List, Callable
 from functools import partial
-import os
 
 # Imports: third party
 import h5py
-import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
 
 # Imports: first party
 from ml4c3.datasets import train_valid_test_datasets
@@ -28,15 +28,15 @@ def most_recent_ecg_date(hd5: h5py.File) -> List[str]:
 
 
 # ECG augmentations
-# TODO: All strengths should reasonably vary between 0 and 1
+# All strengths have a reasonable range between 0 and 1
 def warp(ecg, strength):
-    strength *= 1  # reasonable range between -> [0, 1]
+    strength *= 0.2  # reasonable range between -> [0, 1]
 
     i = np.linspace(0, 1, len(ecg))
     envelope = strength * (0.5 - np.abs(0.5 - i))
     warped = i + envelope * (
-        np.sin(np.random.rand() * 5 + np.random.randn() * 5)
-        + np.cos(np.random.rand() * 5 + np.random.randn() * 5)
+        np.sin(np.random.rand() * 5 * i + np.random.randn() * 5)
+        + np.cos(np.random.rand() * 5 * i + np.random.randn() * 5)
     )
     warped_ecg = np.zeros_like(ecg)
     for j in range(ecg.shape[1]):
@@ -55,7 +55,7 @@ def crop(ecg, strength):
 
 
 def gaussian_noise(ecg, strength):
-    strength *= 10  # reasonable range between -> [0, 1]
+    strength *= 2  # reasonable range between -> [0, 1]
 
     noise_frac = np.random.rand() * strength
     return ecg + noise_frac * np.random.randn(*ecg.shape)
@@ -67,14 +67,14 @@ def roll(ecg, strength):
 
 
 def baseline_drift(ecg, strength):
-    strength *= 1  # reasonable range between -> [0, 1]
+    strength *= 2  # reasonable range between -> [0, 1]
 
     frequency = (
         (np.random.rand() * 20 + 10) * 10 / 60
     )  # typical breaths per second for an adult
     phase = np.random.rand() * 2 * np.pi
     drift = strength * np.sin(np.linspace(0, 1, len(ecg)) * frequency + phase)
-    return ecg + drift
+    return ecg + drift[:, np.newaxis]
 
 
 def augmentation_dict():
@@ -91,14 +91,22 @@ def chain_apply(input_value, functions: List[Callable]):
     return out
 
 
-def apply_augmentation_strengths(augmentation_strengths: Dict[str, float], num_augmentations: int):
+def apply_augmentation_strengths(
+    augmentation_strengths: Dict[str, float],
+    num_augmentations: int,
+):
     aug_dict = augmentation_dict()
+    if num_augmentations == 0:
+        return []
     augmentations = [
         partial(aug_dict[func_name], strength=strength)
         for func_name, strength in augmentation_strengths.items()
     ]
     return [
-        lambda ecg: chain_apply(ecg, np.random.choice(augmentations, num_augmentations))
+        lambda ecg: chain_apply(
+            ecg,
+            np.random.choice(augmentations, num_augmentations),
+        ),
     ]
 
 
@@ -172,7 +180,10 @@ def get_pretraining_datasets(
     valid_csv: str,
     test_csv: str,
 ):
-    augmentations = apply_augmentation_strengths(augmentation_strengths, num_augmentations)
+    augmentations = apply_augmentation_strengths(
+        augmentation_strengths,
+        num_augmentations,
+    )
     ecg_tmap = get_ecg_tmap(ecg_length, augmentations)
     return train_valid_test_datasets(
         [ecg_tmap],
@@ -193,8 +204,8 @@ def demo_augmentation(
     axes: List[plt.Axes],
 ):
     for axis, strength in zip(axes, np.linspace(0, 1, len(axes))):
-        axis.axis('off')  # hide x and y ticks
-        axis.plot(augmentation(ecg, strength))
+        axis.axis("off")  # hide x and y ticks
+        axis.plot(augmentation(ecg, strength)[:, 0], c="k")  # plot only lead I
 
 
 def demo_augmentations(
@@ -203,32 +214,41 @@ def demo_augmentations(
     valid_csv: str,
     test_csv: str,
     output_folder: str,
+    **kwargs,
 ):
     os.makedirs(output_folder, exist_ok=True)
     augmentations = augmentation_dict()
     ecgs_per_augmentation = 3
 
-    (train_dataset, _, _), _, _ = get_pretraining_datasets(
+    (train_dataset, _, _), _, cleanups = get_pretraining_datasets(
         ecg_length=2500,
-        augmentation_strengths={name: 0. for name in augmentations},
+        augmentation_strengths={name: 0.0 for name in augmentations},
         num_augmentations=0,
         hd5_folder=hd5_folder,
-        num_workers=0,
+        num_workers=1,
         batch_size=ecgs_per_augmentation,
         train_csv=train_csv,
         valid_csv=valid_csv,
         test_csv=test_csv,
     )
-    ecgs = train_dataset.take(1)
+    ecgs = next(train_dataset.take(1).as_numpy_iterator())[0]["input_ecg_continuous"]
     for name, augmentation in augmentations.items():
-        fig, axes = plt.subplots(10, ecgs_per_augmentation, figsize=(10 * ecgs_per_augmentation, 10))
+        fig, axes = plt.subplots(
+            10,
+            ecgs_per_augmentation,
+            figsize=(10 * ecgs_per_augmentation, 10),
+        )
         for i, ecg in enumerate(ecgs):
+            np.random.seed(i)
             demo_augmentation(ecg, augmentation, axes[:, i])
-        plt.savefig(os.path.join(output_folder, f'{name}.png'))
+        plt.savefig(os.path.join(output_folder, f"{name}.png"))
+
+    for cleanup in cleanups:
+        cleanup()
 
 
 MODES = {
-    'augmentation_demo': demo_augmentations,
+    "augmentation_demo": demo_augmentations,
     # TODO: add explore
 }
 
@@ -238,7 +258,6 @@ def parse_args():
     parser.add_argument(
         "mode",
         help=f"Which data functionality to use. Choose between {list(MODES)}.",
-        required=True,
         choices=list(MODES),
     )
     parser.add_argument(
@@ -248,16 +267,12 @@ def parse_args():
     )
     parser.add_argument(
         "--valid_csv",
-        help=(
-            "Path to CSV with Sample IDs to reserve for validation"
-        ),
+        help=("Path to CSV with Sample IDs to reserve for validation"),
         required=True,
     )
     parser.add_argument(
         "--test_csv",
-        help=(
-            "Path to CSV with Sample IDs to reserve for testing."
-        ),
+        help=("Path to CSV with Sample IDs to reserve for testing."),
         required=True,
     )
     parser.add_argument(
