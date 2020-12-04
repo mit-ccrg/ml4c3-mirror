fd7b9a225b9306d3fc34a41807795a5c960d1303
Steven Song <steven.song98@gmail.com>
2020-12-04 09:14:54 -0500

    Updates run.sh to disable user site packages in docker container and print formatted docker settings (#918)
    * updates run.sh
    
    * cleanup run
    
    Co-authored-by: Erik Reinertsen <ereinertsen@mgh.harvard.edu>

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,26 +12,25 @@ MOUNTS=""
 PORT="8888"
 PORT_FLAG=""
 PYTHON_COMMAND="python"
+SHELL_COMMAND="bash"
 TEST_COMMAND="python -m pytest"
 JUPYTER_COMMAND="jupyter lab --allow-root"
 VISUALIZER_COMMAND="python $PWD/ml4c3/recipes.py visualize --debug"
 SCRIPT_NAME=$( echo $0 | sed 's#.*/##g' )
 CONTAINER_NAME=""
+ENV_VARS=""
 
 ################### USERNAME & GROUPS ####################################
 
 # Get group ids and names, interleaved: id1 group1 id2 group2 ...
 GROUP_IDS_NAMES=$(id ${USER} | sed 's/.*groups=//g' | awk -F'[,()]' '{for (i=1; i<=NF; i++) {if ($i == "") {continue} print $i}}')
 
-# Export environment variables so they can be passed into Docker and accessed in bash
-export GROUP_IDS_NAMES
-
 # Create string to be called in Docker's bash shell via eval;
 # this creates a user, adds groups, adds user to groups, then calls the Python script
 # the 'staff' group is a preloaded group in the docker image which enables access to root owned folders
 SETUP_USER="
     useradd -u $(id -u) -d ${HOME} ${USER};
-    GROUPS_ARR=( \${GROUP_IDS_NAMES} );
+    GROUPS_ARR=( ${GROUP_IDS_NAMES} );
     for (( i=0; i<\${#GROUPS_ARR[@]}; i=i+2 )); do
         echo \"Creating group\" \${GROUPS_ARR[i+1]} \"with gid\" \${GROUPS_ARR[i]};
         groupadd -f -g \${GROUPS_ARR[i]} \${GROUPS_ARR[i+1]};
@@ -44,51 +43,58 @@ SETUP_USER="
     usermod -aG sudo ${USER};
     echo -e \"password\npassword\" | passwd ${USER};
 "
-CALL_AS_USER="sudo SETUPTOOLS_USE_DISTUTILS=stdlib -H -u ${USER}"
+
+ENV_VARS="$ENV_VARS TF_FORCE_GPU_ALLOW_GROWTH=true"
+ENV_VARS="$ENV_VARS PYTHONNOUSERSITE=true"
+ENV_VARS="$ENV_VARS SETUPTOOLS_USE_DISTUTILS=stdlib"
+
+CALL_AS_USER="sudo $ENV_VARS -H -u ${USER}"
 
 ################### HELP TEXT ############################################
 
 usage()
 {
-    cat <<USAGE_MESSAGE
+    cat << USAGE_MESSAGE
 
     This script can be used to run a Python module within a Docker container.
 
-    Usage: ${SCRIPT_NAME} [-c|-d <gpu_id>] [-m <directory>] [-i <image>] [-j|-T] [-p <port>] [-nrh] module [arg ...]
+    Usage: ${SCRIPT_NAME} [-d <gpu_id>] [-m <directory>] [-i <image>] [-p <port>] [-j|-s|-t|-v] [-nrh] module [arg ...]
 
     Example: ./${SCRIPT_NAME} -n -i ml4c3:latest-cpu recipes.py --mode tensorize ...
 
-        -d                  Select a particular GPU device; first GPU is '0'
+        -d  <gpu_id>     Select a particular GPU device; first GPU is '0'.
 
-        -m                  Directories to mount at the same path in the docker image
+        -m  <directory>  Directories to mount at the same path in the docker image.
 
-        -j                  Run Jupyter Lab
+        -i  <image>      Run Docker with the specified custom <image>. The default image is '${DOCKER_IMAGE}'.
 
-        -p                  Ports to map between docker container and host
+        -p  <port>       Port to map between docker container and host.
 
-        -n                  Run Docker container non-interactively.
+        -j               Run Jupyter Lab.
 
-        -r                  Call Python script as root. If this flag is not specified,
-                            the owner and group of the output directory will be those
-                            of the user who called the script.
+        -s               Run bash shell.
 
-        -h                  Print this help text.
+        -t               Run tests.
 
-        -i      <image>     Run Docker with the specified custom <image>. The default image is '${DOCKER_IMAGE}'.
-        -T                  Run tests
+        -v               Run visualizer.
+
+        -n               Run Docker container non-interactively.
+
+        -r               Call Python script as root. If this flag is not specified,
+                         the owner and group of the output directory will be those
+                         of the user who called the script.
+
+        -h               Print this help text.
 USAGE_MESSAGE
 }
 
 ################### OPTION PARSING #######################################
 
-while getopts ":i:d:m:p:jnrhTv" opt ; do
+while getopts ":i:d:m:p:jstvnrh" opt ; do
     case ${opt} in
         h)
             usage
             exit 1
-            ;;
-        i)
-            DOCKER_IMAGE=$OPTARG
             ;;
         d)
             DOCKER_IMAGE=${DOCKER_IMAGE_GPU}
@@ -98,25 +104,32 @@ while getopts ":i:d:m:p:jnrhTv" opt ; do
             MOUNTS="${MOUNTS} -v ${OPTARG}:${OPTARG}"
             MOUNTED_DIR=${OPTARG}
             ;;
-        j)
-            PYTHON_COMMAND=${JUPYTER_COMMAND}
-            ;;
-        v)
-            PYTHON_COMMAND=${VISUALIZER_COMMAND}
+        i)
+            DOCKER_IMAGE=$OPTARG
             ;;
         p)
             PORT="${OPTARG}"
             PORT_FLAG="-p ${PORT}:${PORT}"
+            ;;
+        j)
+            PYTHON_COMMAND=${JUPYTER_COMMAND}
+            ;;
+        s)
+            PYTHON_COMMAND=${SHELL_COMMAND}
+            ;;
+        t)
+            PYTHON_COMMAND=${TEST_COMMAND}
+            ;;
+        v)
+            PYTHON_COMMAND=${VISUALIZER_COMMAND}
             ;;
         n)
             INTERACTIVE=""
             ;;
         r) # Output owned by root
             SETUP_USER=""
+
             CALL_AS_USER="SETUPTOOLS_USE_DISTUTILS=stdlib"
-            ;;
-        T)
-            PYTHON_COMMAND=${TEST_COMMAND}
             ;;
         :)
             echo "ERROR: Option -${OPTARG} requires an argument." 1>&2
@@ -132,7 +145,10 @@ while getopts ":i:d:m:p:jnrhTv" opt ; do
 done
 shift $((OPTIND - 1))
 
-if [[ $# -eq 0 && "$PYTHON_COMMAND" != "$JUPYTER_COMMAND" && "$PYTHON_COMMAND" != "$VISUALIZER_COMMAND" ]]; then
+if [[ $# -eq 0 && \
+      $PYTHON_COMMAND != $SHELL_COMMAND && \
+      $PYTHON_COMMAND != $JUPYTER_COMMAND && \
+      $PYTHON_COMMAND != $VISUALIZER_COMMAND ]]; then
     echo "ERROR: No Python module was specified." 1>&2
     usage
     exit 1
@@ -140,8 +156,14 @@ fi
 
 ################### SCRIPT BODY ##########################################
 
+# Pull Docker image
 if ! docker pull ${DOCKER_IMAGE}; then
     echo "Could not pull the image ${DOCKER_IMAGE}."
+fi
+
+# Setup mounted directories
+if [[ "$SETUP_USER" ]]; then
+  MOUNTS="${MOUNTS} -v ${HOME}/:${HOME}/"
 fi
 
 if [[ -d "/storage" ]] ; then
@@ -154,7 +176,9 @@ if [[ -d "/media" ]] ; then
     MOUNTS="${MOUNTS} -v /media/:/media/"
 fi
 
+# Setup python command and args
 PYTHON_ARGS="$@"
+
 if [[ "$PYTHON_COMMAND" == "$JUPYTER_COMMAND" ]] ; then
     if [[ "${PORT_FLAG}" == "" ]] ; then
         PORT_FLAG="-p ${PORT}:${PORT}"
@@ -163,6 +187,7 @@ if [[ "$PYTHON_COMMAND" == "$JUPYTER_COMMAND" ]] ; then
     PYTHON_ARGS="--port ${PORT} --ip 0.0.0.0 --no-browser --NotebookApp.custom_display_url http://${FQDN}:${PORT}"
     CONTAINER_NAME="--name ${USER}-notebook-${PORT}"
 fi
+
 if [[ "$PYTHON_COMMAND" == "$VISUALIZER_COMMAND" ]] ; then
     if [[ "${PORT_FLAG}" == "" ]] ; then
         PORT_FLAG="-p ${PORT}:${PORT}"
@@ -171,33 +196,62 @@ if [[ "$PYTHON_COMMAND" == "$VISUALIZER_COMMAND" ]] ; then
     CONTAINER_NAME="--name ${USER}-visualizer-${PORT}"
 fi
 
-cat <<LAUNCH_MESSAGE
-Attempting to run Docker with
-    docker run --rm \
-    ${INTERACTIVE} \
-    ${GPU_DEVICE} \
-    --env GROUP_IDS_NAMES \
-    --env TF_FORCE_GPU_ALLOW_GROWTH=true \
-    --uts=host \
-    --ipc=host \
-    -v ${HOME}/:${HOME}/ \
-    ${MOUNTS} \
-    ${PORT_FLAG} \
-    ${CONTAINER_NAME} \
-    ${DOCKER_IMAGE} /bin/bash -c \
+# Setup heredoc
+if [[ "$GPU_DEVICE" ]]
+then
+    GPU_DEVICE_STRING="$GPU_DEVICE "
+else
+    GPU_DEVICE_STRING="# no GPU device specified"
+fi
+
+if [[ "$MOUNTS" ]]
+then
+    MOUNTS_STRING="$MOUNTS"
+else
+    MOUNTS_STRING="# no mount points specified"
+fi
+
+if [[ "$PORT_FLAG" ]]
+then
+    PORT_FLAG="$PORT_FLAG"
+else
+    PORT_FLAG_STRING="# no ports specified"
+fi
+
+if [[ "$CONTAINER_NAME" ]]
+then
+    CONTAINER_NAME_STRING="$CONTAINER_NAME"
+else
+    CONTAINER_NAME_STRING="# container name not specified"
+fi
+
+cat << EOF
+
+Attempting to run Docker with:
+    docker run --rm \\
+    ${INTERACTIVE} \\
+    ${GPU_DEVICE_STRING} \\
+    --uts=host \\
+    --ipc=host \\
+    ${MOUNTS_STRING} \\
+    ${PORT_FLAG_STRING} \\
+    ${CONTAINER_NAME_STRING} \\
+    ${DOCKER_IMAGE} /bin/bash -c \\
     "${SETUP_USER}
     cd ${HOME};
+    ${CALL_AS_USER} pip install $PWD;
     ${CALL_AS_USER} ${PYTHON_COMMAND} ${PYTHON_ARGS}"
-LAUNCH_MESSAGE
+
+EOF
+
+# Run Docker
+echo "=================== Running Docker ==================="
 
 docker run --rm \
 ${INTERACTIVE} \
 ${GPU_DEVICE} \
---env GROUP_IDS_NAMES \
---env TF_FORCE_GPU_ALLOW_GROWTH=true \
 --uts=host \
 --ipc=host \
--v ${HOME}/:${HOME}/ \
 ${MOUNTS} \
 ${PORT_FLAG} \
 ${CONTAINER_NAME} \
