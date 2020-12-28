ECG voltage normalization and explore bug fix (#1038)
* bug fix

* no verbose

* ECG voltage normalization

* format

* reorder regex

* Update ml4c3/normalizer.py

Co-authored-by: Erik Reinertsen <erikr@mit.edu>

* bug fix

* disable numexpr warning and fix typo in models

Co-authored-by: Erik Reinertsen <erikr@mit.edu>

ml4c3/explorations.py
@@ -1206,10 +1206,17 @@ def _modify_tmap_to_return_mean(tmap: TensorMap) -> TensorMap:
 
     def tff(_: TensorMap, data: PatientData):
         _tensor = tmap.tensor_from_file(tmap, data)
-        _tensor = tmap.postprocess_tensor(_tensor, data, augment=False)
         if tmap.time_series_limit is None:
-            return np.array([_tensor.mean()])
-        return _tensor.mean(axis=tuple(range(len(_tensor.shape)))[1:])[:, None]
+            _tensor = np.array([_tensor])
+        for i in range(len(_tensor)):
+            try:
+                _tensor[i] = tmap.postprocess_tensor(_tensor[i], data, augment=False)
+            except:
+                _tensor[i] = np.nan
+        tensor = _tensor.mean(axis=tuple(range(len(_tensor.shape)))[1:])[:, None]
+        if tmap.time_series_limit is None:
+            tensor = tensor[0]
+        return tensor
 
     new_tm.tensor_from_file = tff
     return new_tm

ml4c3/normalizer.py
@@ -26,7 +26,7 @@ class Normalizer(ABC):
         return tensor
 
 
-class Standardize(Normalizer):
+class ZScorePopulation(Normalizer):
     """
     Normalize a tensor by subtracting a constant mean and dividing by a
     constant standard deviation.
@@ -42,7 +42,7 @@ class Standardize(Normalizer):
         return tensor * (self.std + EPS) + self.mean
 
 
-class RobustScaler(Normalizer):
+class RobustScalePopulation(Normalizer):
     """
     Normalize a tensor by subtracting a constant median and dividing by a
     constant interquartile range.
@@ -58,7 +58,7 @@ class RobustScaler(Normalizer):
         return tensor * (self.iqr + EPS) + self.median
 
 
-class ZeroMeanStd1(Normalizer):
+class ZScore(Normalizer):
     """
     Normalize a tensor by subtracting the mean of the tensor and dividing by
     the standard deviation of the tensor.
@@ -68,6 +68,19 @@ class ZeroMeanStd1(Normalizer):
         tensor -= np.mean(tensor)
         tensor /= np.std(tensor) + EPS
         return tensor
+
+
+class RobustScale(Normalizer):
+    """
+    Normalize a tensor by subtracting the median of the tensor and dividing by the
+    interquartile range of the tensor.
+    """
+
+    def normalize(self, tensor: np.ndarray) -> np.ndarray:
+        q25, q75 = np.percentile(tensor, [25, 75])
+        iqr = q75 - q25
+        median = np.median(tensor)
+        return (tensor - median) / (iqr + EPS)
 
 
 class MinMax(Normalizer):