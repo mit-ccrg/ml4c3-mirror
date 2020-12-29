2d43a3bbbc601aaf432a011be9e03c7cf8d20b6c
Roger <52486315+RogerPallares@users.noreply.github.com>
2020-12-29 12:05:45 -0500

    deletes unnecessary adt columns. Ref #990 (#1045)

--- ingest/edw/queries-pipeline/adt.sql
+++ ingest/edw/queries-pipeline/adt.sql
@@ -1,28 +1,18 @@
 SELECT distinct
     t3.PatientIdentityID as MRN
     ,t1.PatientEncounterID
-    ,t1.RoomID
     ,t5.RoomNM
-    ,t1.BedID
     ,t6.BedLabelNM
     ,t4.HospitalAdmitDTS
     ,t4.HospitalDischargeDTS
     ,t2.EffectiveDTS as TransferInDTS
     ,t1.EffectiveDTS as TransferOutDTS
-    ,t1.ADTEventTypeCD
     ,t1.ADTEventTypeDSC
-    ,t1.ADTEventSubtypeCD
-    ,t1.ADTEventSubtypeDSC
     ,t1.DepartmentID
     ,t1.DepartmentDSC
-    ,t1.EffectiveDTS
     ,t1.EventDTS
-    ,t1.PatientClassCD
     ,t1.PatientClassDSC
-    ,t1.PatientServiceCD
     ,t1.PatientServiceDSC
-    ,t1.OriginalEventSubtypeDTS
-    ,t1.OriginalEffectiveDTS
 FROM
     Epic.Encounter.ADT_MGH t1
     join Epic.Encounter.ADT_MGH t2 on t1.LastInADTEventID=t2.EventID
