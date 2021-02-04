65ec898e7181712b16721d9645b12dfd041d58fd
Eric Palanques <61511203+epalto@users.noreply.github.com>
2021-02-04 11:41:37 -0500

    Ignore certain numbers on sdc git hook (#1303)
    * Added possibility to ignore specific numbers on sdc hook
    
    * Adjusted line number
    
    * More detailed bad result hook message

--- scripts/sensitive_data_githook.py
+++ scripts/sensitive_data_githook.py
@@ -8,8 +8,14 @@ from typing import Optional, Sequence
 
 
 def get_mrns_and_csns(argv: Optional[Sequence[str]] = None):
+    """
+    Detects all numbers of 4 or more digits on the committed files.
+    Numbers preceded with 4 or more consecutive digits are ignored
+    Numbers on the `--ignore_numbers` param are ignored too
+    """
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("--ignore_numbers", nargs="+", help="Ignore certain numbers.")
     parser.add_argument("filenames", nargs="*", help="Filenames to check.")
     args = parser.parse_args(argv)
 
@@ -27,15 +33,18 @@ def get_mrns_and_csns(argv: Optional[Sequence[str]] = None):
                     re.MULTILINE,
                 )
                 for data in sensitive_data:
-                    print(
-                        f"{filename} - line {line_num}: "
-                        f"Possible sensitive data: {data.group()}",
-                    )
-                    retval = 1
+                    if data.group() not in args.ignore_numbers:
+                        print(
+                            f"{filename} - line {line_num+1}: "
+                            f"Possible sensitive data: {data.group()}",
+                        )
+                        retval = 1
     if retval:
         print(
-            "If you believe have checked that this is not sensitive data, "
-            "you can skip this hook by running 'SKIP=sdc git commit -m <msg>'",
+            "If you have checked that this is not sensitive data, "
+            "you can skip a line using '# sdc: nsd', the rest of a document "
+            "using '#sdc: nsd-stop' or skip this hook by running "
+            "'SKIP=sdc git commit -m <msg>' ",
         )
     return sys.exit(retval)
 
